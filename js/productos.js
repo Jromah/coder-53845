c3956747953ecbec3d285afb5825b1b97aa1783d
Jean Rodriguez <91.jroma@gmail.com>
2024-06-30 17:55:49 -0300

    js form

--- js/productos.js
+++ js/productos.js
@@ -1,22 +1,25 @@
-const productos = [
-    { nombre: 'Campera Ferrari 2023', precio: 2500 },
-    { nombre: 'Campera RedBull 2023', precio: 2000 },
-    { nombre: 'Campera Mercedes 2022', precio: 1500 },
-    { nombre: 'Gorra Mercedes 2023', precio: 200 },
-    { nombre: 'Gorra Leclerc 2024', precio: 3000 },
-    { nombre: 'Gorra RedBull 2024', precio: 2700 }
-];
+// const productos = [
+//     { nombre: 'Campera Ferrari 2023', precio: 2500 },
+//     { nombre: 'Campera RedBull 2023', precio: 2000 },
+//     { nombre: 'Campera Mercedes 2022', precio: 1500 },
+//     { nombre: 'Gorra Mercedes 2023', precio: 200 },
+//     { nombre: 'Gorra Leclerc 2024', precio: 3000 },
+//     { nombre: 'Gorra RedBull 2024', precio: 2700 }
+// ];
 
-let carrito = [];
-let seleccion = '';
-do {
-    seleccion = prompt('Elige un producto y cuando quieras terminar la compra escribe finalizar:\n' + productos.map((producto, i) => `${i + 1}. ${producto.nombre}`).join('\n') );
-    if (seleccion !== 'finalizar') {
-        const cantidad = parseInt(prompt('¿Cuántas unidades quieres?'), 10);
-        const producto = productos[parseInt(seleccion, 10) - 1];
-        carrito.push({ ...producto, cantidad });
-    }
-} while (seleccion !== 'finalizar');
+// let carrito = [];
+// let seleccion = '';
+// do {
+//     seleccion = prompt('Elige un producto y cuando quieras terminar la compra escribe finalizar:\n' + productos.map((producto, i) => `${i + 1}. ${producto.nombre}`).join('\n') );
+//     if (seleccion !== 'finalizar') {
+//         const cantidad = parseInt(prompt('¿Cuántas unidades quieres?'), 10);
+//         const producto = productos[parseInt(seleccion, 10) - 1];
+//         carrito.push({ ...producto, cantidad });
+//     }
+// } while (seleccion !== 'finalizar');
 
-const total = carrito.reduce((acum, { precio, cantidad }) => acum + precio * cantidad, 0);
-alert(`El total de tu compra es: $${total}` + ' pesos.');
+// const total = carrito.reduce((acum, { precio, cantidad }) => acum + precio * cantidad, 0);
+// alert(`El total de tu compra es: $${total}` + ' pesos.');
+
+// aqui inicia la 3era entrega, no borre la entrega anterior para poder tenerla a mano y practuicar luego
+
